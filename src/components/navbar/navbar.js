4db7d607edaa54bbb682333d4c18a885d97dd97a
Matt Smith <mrmcchow@gmail.com>
2019-02-23 01:11:23 +0000

    Started Navbar

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './navbar.css'
+
+const Navbar = () => (
+    <nav className="navbar">
+        <a class="navbar-title" href="/">Rustys Resources</a>
+    </nav>
+);
+
+export default Navbar;
