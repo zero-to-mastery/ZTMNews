c1e0dd29df7c50d1ec79acd87911633e552e90cd
Matt Smith <mrmcchow@gmail.com>
2019-02-24 03:20:36 +0000

    Card optimisations

--- src/components/cards/card.js
+++ src/components/cards/card.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import './card.css'
 
-const Card = ({ resource, active }) => {
+const Card = ({ resource, active, upvotes }) => {
 
     const imgStyle = {
         backgroundImage: `url(${resource.image})`
     };
 
     return (
-        <div className={`card ${(active ? "cardActive" : "")}`}>
-            <div className="cardImg" style={imgStyle}>
+        <div className="card">
+
+            
+
+            <div>
+                <div className="cardImg" style={imgStyle}>
+
+                </div>
+                <div className="resOptions">
+                    <div className="cardIcon"><i className="far fa-thumbs-up"></i> 155</div>
+                    <div className="cardIcon"><i className="fas fa-thumbtack"></i> 15</div>
+                </div>
             </div>
+
+
             <div className="cardDesc">
                 <h2>{resource.title}</h2>
                 <p>{resource.description}</p>
--- src/components/cards/cardList.js
+++ src/components/cards/cardList.js
@@ -3,24 +3,21 @@ import Card from './card';
 
 const CardList = ({ resources }) => {
   return (
-    <div>
+    <section id="cardContainer">
       {
         resources.map((resource, i) => {
-          // to demonstrate bookmarked and unbookmarked
-          const bookClass = (i % 2 === 0 ? true : false)
           return (
-            <section id="cardContainer">
-            <div className={`bookmark ${(bookClass ? "bookmarkActive" : "")}`}><i class="fas fa-bookmark"></i></div>
+
               <Card 
                 key={i}
                 resource={resource.meta}
-                active={bookClass}
+                upvotes={resource.upvotes}
               />
-            </section>
+           
           );
         })
       }
-    </div>
+    </section>
   );
 }
 
