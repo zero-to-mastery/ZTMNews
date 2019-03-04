87df7f0e9bbde516c336c0412dd6a40964c3cd2d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-04 12:24:30 +0000

    Deploy zero-to-mastery/ZTMNews to github.com/zero-to-mastery/ZTMNews.git:gh-pages

---
+++ precache-manifest.47c9eb3423cb4e8267e775d70e7e69cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "02754719fae35dd4dca2",
+    "url": "/ZTMNews/static/css/main.7ca0f1d7.chunk.css"
+  },
+  {
+    "revision": "02754719fae35dd4dca2",
+    "url": "/ZTMNews/static/js/main.02754719.chunk.js"
+  },
+  {
+    "revision": "6f072c6930a3b6821da8",
+    "url": "/ZTMNews/static/js/runtime~main.6f072c69.js"
+  },
+  {
+    "revision": "f1e94f3741b066b8a0aa",
+    "url": "/ZTMNews/static/js/2.f1e94f37.chunk.js"
+  },
+  {
+    "revision": "246dc68200303d08c1c2bfc903fe8185",
+    "url": "/ZTMNews/static/media/rusty.246dc682.png"
+  },
+  {
+    "revision": "15e5a12e3f45ecc2f0a00124bb16a562",
+    "url": "/ZTMNews/index.html"
+  }
+];
